defdaa33bf5a381aeb51dda5a57fb2f8353da6f3
magdy114 <yusufmagdi17@gmail.com>
2026-07-04 11:19:29 +0400

    Enable expert planner modules

--- expert_entry.py
+++ expert_entry.py
@@ -1,1 +1,10 @@
-from app import app
+import app as core
+import lesson_engine
+import word_engine
+
+def content_for(lesson):
+    return lesson_engine.build_expert_content(lesson,core)
+
+vars(core).update({'build_content':content_for})
+word_engine.install_word_upgrade(core)
+app=core.app
